b1574e531bfec1683c5811fc63c1117910c00dd2
GetGenryGit <genrygit@gmail.com>
2024-02-27 22:58:48 +0300

    first commit

--- src/HotelProject.Client/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Client.AssemblyInfo.cs
+++ src/HotelProject.Client/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Client.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cd328900368ba9cef4199c1b8786dd009eb0f174")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
